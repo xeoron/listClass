106fdaa4d286cf2c8d8b05dc34eca18124ded9fc
Jason Campisi <aitsinformation@gmail.com>
2013-01-21 14:32:23 -0500

    v0.1.4

---
+++ README.md
@@ -0,0 +1,81 @@
+ListClass.pl
+========
+Given X number of sourcecode files display a list of all the classes and subroutines declarations found. This is a 
+quick and dirty script. Currently, the code looks patterns for function\method calls regardless of checking the 
+language in the file, but if there is a next version it will have language detection and act accordingly.
+
+Some of the Supported Languages
+	
+	Applescript
+	Awk
+	Bash
+	C
+	C++
+	Dart
+	Go
+	Lisp
+	Fortran
+	Java
+	Javascript
+	MatLab
+	Perl
+	PHP
+	Python
+	Ruby
+	Scheme
+	TCL
+	Visual Basic
+	
+Limitations
+
+	Missing detection of functions that use structs or generics. 
+	Example of what it can't detect: FOO Bar(FOO T)	
+
+Usage
+=====
+    
+	listClass.pl file1.py file2.pl file3.java file4.sh
+    
+   Example reading 3 different files
+  
+     listClass.pl woof.py frenamer.pl twitterstream.py
+    
+   Results
+
+     ..\woof.py:(7)
+	 ===========
+ 	  def find_ip ():
+ 	  class FileServHTTPRequestHandler (BaseHTTPServer.BaseHTTPRequestHandler):
+  	  def log_request (self, code='-', size='-'):
+  	  def do_GET (self):
+ 	  def serve_files (filename, maxdown = 1, ip_addr = '', port = 8080):
+ 	  def usage (defport, defmaxdown, errmsg = None):
+ 	  def main ():
+   
+     ..\frenamer\frenamer.pl:(15)
+	 ========================
+	  sub sig_handler{ 	#capture Ctrl+C signal
+      sub cmdlnParm(){	#display the program usage info 
+      sub confirmChange($$){ #ask if pending change is good or bad. Parameters $currentFilename and $newFilename
+      sub getPerms($){ 	#get file permisions in *nix format. Parameter=$file to lookup
+      sub fRename($){ 	#file renaming... only call this when not crawling any subfolders. Parameter = $folder to look at
+      sub _transLC($){ 	#take a tokenized char and make it lower case
+      sub _transLC($) { 	#make-lower-case
+      sub _transToken($){	 #filter token
+      sub _translateWL($){    #translate 1st letter of each word to uppercase
+      sub _translate($){	#translate case either up or down. Parameter = $file
+      sub _rFRename($){ 	#recursive file renaming processing. Parameter = $file
+      sub _untaintData ($$){	#dereference any reserved non-word characters. Parameter = string of data to untaint, flag
+      sub untaintData(){		#sanitize provided input data
+      sub bool ($){	return (shift >=1) ? "On" : "Off";  } #translate boolean to On or Off for user verbose settings mode
+      sub main(){
+   
+    .\twitterStream.py:(6)
+	===================
+      class TwitterStream:
+      def __init__(self):
+      def setup_connection(self):
+      def get_oauth_header(self):
+      def start(self):
+      def handle_tweet(self, data):
+	
---
+++ listClass.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl
+# Author: Jason Campisi
+# File: classDataList.pl
+# CopyLeft: 2002
+#
+# Purpose: create a program that will read in X number of filenames and display
+#  all the data in the file that is listed as public and private data and methods.
+#  Why? Because text environments, such as, Vim lacks the ability to list them in text-box to the side like most IDE's.
+#
+# System requirements: Perl 5 or higher. Should be platform independent. 
+# Tested on Perl 5.8.7 on Knoppix Linux 4.1+ & KUbuntu Linux 5.10+
+#
+#NOTE: This is a quick and dirty script. Currently, the code looks for 
+# function\method calls, regarless of language, but if there is a next 
+# version it will have language detection and act accordingly. Until such time
+# this is good enough for displaying method declarations in a terminal screen and 
+# using VIM in another
+
+use strict;
+use Fcntl;
+use constant LOCK_UN => '8';
+use constant LOCK_EX => '2';
+my $version ="v0.1.4 Date(9/17/2002)";
+
+sub main(){
+ usage() if( scalar(@ARGV)<=0 or ($ARGV[0]=~m/-(v|-version|h|-help)/i));
+ 
+ my @result; 
+ my $count=0;
+ my %pattern = (
+	def => qr/^\s*def\s+/,					#python, ruby
+	sub => qr/^\s*sub\s+/,					#perl, visual basic
+	class => qr/^\s*class\s+/,				#C, C++, Java, C#, Dart
+	function1 => qr/^\s*function\s*\w+[\(|\{]/,	#javascript, php awk, bash, Lua, visual basic
+	function2 => qr/^\s*function\s+/,			#awk, bash
+	function3 => qr/^\s*function\s+\w+=\w+\(/,	#matlab
+	func => qr/^\s*func\s*\w+\(/,			#Go
+	on => qr/^\s*on\s*\w+\(/, 				#applescript
+	onrun => qr/^\s*on\s*run/	, 			#applescript
+	onopen=> qr/^\s*on\s*open/, 			#applescript droplet method
+	private => qr/^\s*private\s+/,			#C, C++, Java, C#
+	public => qr/^\s*public\s+/,			#C, C++, Java, C#
+	protected => qr/^\s*protected\s+/,		#C, C++, Java, C#
+	abstract => qr/^\s*abstract\s*\w+/,		#C, C++, Java, C#
+	defun => qr/^\s*defun\s+/,
+	define => qr/^\s*\(define\s+/,		#Lisp, Scheme
+	int =>qr/^\s*int\s*\w+\(/,			#C,C++, Dart
+	void =>qr/^\s*void\s*\w+\(/,		#C,C++, Dart
+	float =>qr/^\s*float\s*\w+\(/,		#C,C++, Dart
+	string =>qr/^\s*string\s*\w+\(/,	#C,C++, Dart
+	double =>qr/^\s*double\s*\w+\(/,	#C,C++, Dart
+	char =>qr/^\s*char\s*\w+\(/,		#C,C++, Dart
+	static => qr/^\s*static\s*/,		#C++, Dart
+	procedure => qr/^\s*procedure\s+/,
+	proc => qr/^\s*proc\s+/,			#tcl
+	subroutine => qr/^\s*subroutine\s+\w+\s*\(/, 					#fortran
+	recursivesubroutine =>qr/^\s*recursive subroutine\s+\w+\s*\(/,	#fortran
+	
+	#note: add Opa support
+	#missing detection of C-based langauge functions that use structs or generics. Exmaple: FOO Bar(FOO f)
+	#language list: http://home.nvg.org/~sk/lang/lang.html
+);
+
+ while (my $file=shift @ARGV){
+    if (-e $file && (-r $file) && !(-d $file)){#exists and not a directory
+		$count=0; #track the # of method declarations found per file
+		@result=();
+    	$result[0]="$file:";
+
+		if (open (DATA,"<$file")){
+			until (flock(DATA, LOCK_EX)){ sleep .10; }
+		}else {  print " Error--Could not read the file $file: $!\n"; next;} 
+		foreach(<DATA>){
+	      next if ( m/^\s?$/ );		  #if blank line, go to the next
+		  s/^\s+/ / if (m/^\s+/);     #prune whitespaces before characters
+	      s/\r$//g  if (m/\r\n$/g && $^O!=~m/mswin/i); #*nix doesn't need \r's
+		  foreach my $k (keys %pattern){ #only 1 value needs to match
+		    if ( m/$pattern{$k}/i ){
+			   $count++;
+			   $result[++$#result]=" $_";
+			   last;
+	       }#end if
+		  }#end foreach
+		}#end foreach
+		flock (DATA, LOCK_UN);
+		
+		$result[0] = $result[0] . "($count)\n" . '=' x length($file . "1") . "\n";
+		foreach (@result){ print;} 
+		print "\n" if ($#ARGV>=0); #add a blank newline between the current and next file to parse for methods
+		undef @result;
+		
+    }else{ 
+		print "File: '" .$file . "' can not be read or does not exist!\n"; 
+	}
+	
+	#reset variables for next loops
+ }#end while
+}#end main
+
+sub usage(){
+  if ($ARGV[0]=~m/-(v|-version)/i){
+	  print "ClassDataList.pl $version\n"; 
+  }else{
+ print <<eof
+ClassDataList.pl $version handles n-number of file names.
+ Usage: perl programName filename1 filename2
+   classDataList.pl <file_1 file_2 ... file_n>
+  --help -h 		displays usage 
+  --version -v		program version
+
+ Searches for most class and methods declarations/headers in
+      Ada, AWK, C?, C++, C#, Fortran, Java, Lisp, Pascal, Perl, 
+      PHP, Pike?, Python, Ruby, Scheme, Tcl, & VB
+ Note: It does not check what language it is parsing. 
+       ? marks denotes known partial support.
+ ClassDataList $version is released under the LGPL version 2 or higher
+eof
+; 
+ } #end else
+ exit (1);
+} #end usage
+
+main();		#start program
+
+__END__
